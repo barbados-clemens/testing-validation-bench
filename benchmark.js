2b0296b3eddc57d1cea5131cc8d302cb4c1dc586
Caleb Ukle <caleb@nrwl.io>
2022-08-03 15:09:40 -0500

    ci

--- benchmark.js
+++ benchmark.js
@@ -1,62 +1,102 @@
-const os = require('os');
+const ghActionCore = require('@actions/core');
 const cp = require('child_process');
 
-
-  function run(cmd,args) {
-    return cp.spawnSync(
-     cmd,
-      args,
-      { stdio: 'inherit', env: { ...process.env, NX_TASKS_RUNNER_DYNAMIC_OUTPUT: 'false' },cwd: process.cwd() }
-    );
-  }
-
-function logIt(msg) {
-  console.log('-----------------------------------')
-  console.log(msg)
-  console.log('-----------------------------------')
+function formatTable(results) {
+  results.sort((a, b) => (a.mean > b.mean ? 1 : -1));
+  const fastest = results[0];
+  return [
+    [
+      { data: 'command', header: true },
+      { data: 'mean', header: true },
+      { data: 'min', header: true },
+      { data: 'max', header: true },
+      { data: 'relative', header: true },
+    ],
+    ...results.map((r) => [
+      `\`${r.command}\``,
+      r.mean,
+      r.min,
+      r.max,
+      Number((r.mean / fastest.mean).toFixed(2)),
+    ]),
+  ];
 }
 
+function run(cmd, args) {
+  return cp.spawnSync(cmd, args, {
+    stdio: 'inherit',
+    env: { ...process.env, NX_TASKS_RUNNER_DYNAMIC_OUTPUT: 'false' },
+    cwd: process.cwd(),
+  });
+}
 
-// run in band
-// hyperfine "npx jest --clear-cache && npx nx run-many --target=test --all --runInBand --skip-nx-cache
+function logIt(msg) {
+  console.log('-----------------------------------');
+  console.log(msg);
+  console.log('-----------------------------------');
+}
+
 const commonArgs = [
-  '-L','jestArgs', '--runInBand,--max-workers=3',
-  '-L', 'nxArgs', '--parallel=false,--parallel=3'];
-  const commonCmds = [
-    'npx nx run-many --target=test --all {jestArgs} {nxArgs} --skip-nx-cache',
-    'jest {jestArgs}'];
-logIt('Starting w/o cache benchmarks')
-run('hyperfine', [
-  '-m', 1,
-  // clear jest cache before each run
-  '-p', 'npx jest --clear-cache',
-  // prep args for each run
-  ...commonArgs,
-  // run nx w/ args
-  ...commonCmds,
-  '--export-json=benchmark-no-cache.json']);
-  // run 8 benchmarks for each arg in the lists * number of commands. Each benchmark runs 10 times.
-  // 1. nx --runInBand --parallel=false
-  // 2. jest --runInBand
-  // 3. nx --max-workers=3 --parallel=false
-  // 4. jest --max-workers=3
-  // 5. nx --runInBand --parallel=3
-  // 6. jest --runInBand
-  // 7. nx --max-workers=3 --parallel=3
-  // 8. jest --max-workers=3
+  '-L',
+  'jestArgs',
+  '--runInBand,--max-workers=3',
+  '-L',
+  'nxArgs',
+  '--parallel=false,--parallel=3',
+];
+const commonCmds = [
+  'npx nx run-many --target=test --all {jestArgs} {nxArgs} --skip-nx-cache',
+  'jest {jestArgs}',
+];
 
-logIt('Starting w/ cache benchmarks')
-run('hyperfine', [
-  '-m', 1,
-  // warm jest cache before runs
-  '-w', 1,
+logIt('Starting w/o cache benchmarks');
+const noCacheArgs = [
+  '-m',
+  1,
+  // clear jest cache before each run
+  '-p',
+  'npx jest --clear-cache',
   // prep args for each run
   ...commonArgs,
   // run nx w/ args
   ...commonCmds,
-  '--export-json=benchmark-cache.json']);
+  '--export-json=benchmark-no-cache.json',
+  '--export-markdown=benchmark-no-cache.md',
+];
+run('hyperfine', noCacheArgs);
+logIt('Starting w/ cache benchmarks');
+const cachedArgs = [
+  '-m',
+  1,
+  // warm jest cache before runs
+  '-w',
+  1,
+  // prep args for each run
+  ...commonArgs,
+  // run nx w/ args
+  ...commonCmds,
+  '--export-json=benchmark-cache.json',
+  '--export-markdown=benchmark-cache.md',
+];
+run('hyperfine', cachedArgs);
 
 const withoutCacheResults = require('./benchmark-no-cache.json');
 const withCacheResults = require('./benchmark-cache.json');
 
-// TODO(caleb) do stuff with benchmark json)
+if (process.env.GITHUB_STEP_SUMMARY) {
+  ghActionCore.summary
+    .addHeading('Benchmark results')
+    .addBreak()
+    .addHeading('No cache', 2)
+    .addCodeBlock(noCacheArgs.join(' '), 'bash')
+    .addTable(formatTable(withoutCacheResults.results))
+    .addHeading('With cache', 2)
+    .addTable(formatTable(withCacheResults.results))
+    .write()
+    .then()
+    .catch((err) => {
+      console.error('Unable to write summary', err);
+    });
+} else {
+  console.log('GITHUB_STEP_SUMMARY not set');
+}
