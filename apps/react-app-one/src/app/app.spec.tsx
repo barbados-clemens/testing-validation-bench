4292b3265af43e46d07a33214968140fe9443996
Caleb Ukle <caleb@nrwl.io>
2022-08-05 16:05:34 -0500

    add example of using @testing-library/jest-dom

--- apps/react-app-one/src/app/app.spec.tsx
+++ apps/react-app-one/src/app/app.spec.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-
+import { screen } from '@testing-library/dom';
 import App from './app';
 
 describe('App', () => {
@@ -12,5 +12,14 @@ describe('App', () => {
     );
 
     expect(baseElement).toBeTruthy();
+  });
+  it('should use jest-dom from testing library', () => {
+    document.body.innerHTML = `
+    <span data-testid="not-empty"><span data-testid="empty"></span></span>
+    <div data-testid="visible">Visible Example</div>
+  `;
+
+    expect(screen.queryByTestId('not-empty')).not.toBeEmptyDOMElement();
+    expect(screen.getByText('Visible Example')).toBeVisible();
   });
 });
--- jest.preset.js
+++ jest.preset.js
@@ -1,7 +1,8 @@
 const nxPreset = require('@nrwl/jest/preset').default;
-
+const path = require('path');
 module.exports = {
   ...nxPreset,
+  setupFilesAfterEnv: [path.resolve(__dirname, './setup-testing-library.js')],
   moduleNameMapper: {
     '^d3$': 'node_modules/d3/dist/d3.min.js',
   },
