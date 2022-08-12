86095f2c50236edf4759216a05d3336df0043f15
Caleb Ukle <caleb@nrwl.io>
2022-08-12 15:01:22 -0500

    add DTE

--- apps/ng-app-one-e2e/src/e2e/env.cy.ts
+++ apps/ng-app-one-e2e/src/e2e/env.cy.ts
@@ -1,17 +1,26 @@
 describe('env vars', () => {
   it('should have cli args', () => {
-    assert.equal(Cypress.env('cliArg'), 'i am from the cli args');
+    assert.equal(
+      Cypress.env('cliArg'),
+      'i am from the cli args',
+      '--env.cliArg value is not as expected!'
+    );
   });
 
   it('should have cypress.env.json vars', () => {
     assert.equal(
       Cypress.env('cypressEnvJson'),
-      'i am from the cypress.env.json file'
+      'i am from the cypress.env.json file',
+      'cypress.env.json value is not as expected!'
     );
   });
 
   // this shouldn't be here because using --env.<whatever> will override the whole env object (expected behavior)
   it('should not have project.json vars', () => {
-    assert.equal(Cypress.env('projectJson'), undefined);
+    assert.equal(
+      Cypress.env('projectJson'),
+      undefined,
+      'project.json value is not as expected!'
+    );
   });
 });
