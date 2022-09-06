7f7d946e8af3cc702e6dfd7d959bd946dcb44a33
Caleb Ukle <caleb@nrwl.io>
2022-09-06 13:00:48 -0500

    update nx

--- libs/react-lib-one/jest.config.ts
+++ libs/react-lib-one/jest.config.ts
@@ -3,7 +3,7 @@ export default {
   displayName: 'react-lib-one',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/react-lib-one',
