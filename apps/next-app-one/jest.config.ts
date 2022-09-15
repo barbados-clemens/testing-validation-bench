bbb07f10bbac56b9dda330a88309191746b09474
Caleb Ukle <caleb@nrwl.io>
2022-09-15 10:02:30 -0500

    fix next lib transformer

--- apps/next-app-one/jest.config.ts
+++ apps/next-app-one/jest.config.ts
@@ -2,11 +2,15 @@
 export default {
   displayName: 'next-app-one',
   preset: '../../jest.preset.js',
+  // blah I'm a comment
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   transformIgnorePatterns: ['/node_modules/(?!nanoid/)'],
+  /**
+   * I sure am a comment
+   */
   coverageDirectory: '../../coverage/apps/next-app-one',
 };
