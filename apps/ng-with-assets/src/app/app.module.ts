c2c87ae85fd11d0247961d085596a0e172be9b6c
Caleb Ukle <caleb.ukle+github@pm.me>
2022-11-14 14:02:45 -0600

    new-ng-app

---
+++ apps/ng-with-assets/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.routes';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    ButtonModule,
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ libs/ct-ng-lib/README.md
@@ -0,0 +1,7 @@
+# ct-ng-lib
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test ct-ng-lib` to execute the unit tests.
